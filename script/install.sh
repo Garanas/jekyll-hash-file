13004a9fc6096dc4e39a64e8b0abcd529f5d203e
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-01-01 11:36:52 +0100

    Cleanup unused scripts

--- script/install.sh
+++
@@ -1,6 +0,0 @@
-task :build do
-  gemspec = 'my_gem.gemspec'
-  system("gem build #{gemspec}")
-  version = Gem::Specification::load(gemspec).version
-  File.rename("my_gem-#{version}.gem", "custom_name.gem")
-end
